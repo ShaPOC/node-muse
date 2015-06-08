2bce852fc56bf410f4bd986420b39a0a34675a97
Jimmy Aupperlee <j.aup.gt@gmail.com>
2015-06-08 13:02:53 +0200

    Another attempt to fix the travis muse installation

--- bin/install_muse.sh
+++ bin/install_muse.sh
@@ -6,8 +6,8 @@ type muse-io >/dev/null 2>&1 || {
     # If not, try to install it
     echo >&2 "muse-io binary not found, trying to install it...";
     wget http://storage.googleapis.com/ix_downloads/musesdk-3.4.1/musesdk-3.4.1-linux-installer.run;
-    chmod a+x musesdk-3.4.1-linux-installer.run;
-    bash ./musesdk-3.4.1-linux-installer.run;
+    sudo chmod a+x musesdk-3.4.1-linux-installer.run;
+    sudo ./musesdk-3.4.1-linux-installer.run;
     rm -rf install.sh;
 
 }
